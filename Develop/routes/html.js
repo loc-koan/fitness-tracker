8ce8df1198e9c40819ac0707566f099a900fdc27
loc-koan <loc.koan@yahoo.com>
2020-08-12 21:49:35 -0500

    html routes file

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -1,7 +1,26 @@
-/* routing path to html pages / files */
+/* routing path to html pages */
 
 const path = require('path');
 
 module.exports = function (app) {
     
-}
+    // routes to index page
+    app.get('/', (request, response) => {
+        response.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    // routes to exercise page
+    app.get('exercise', (request, response) => {
+        response.sendFile(path.join(__dirname, '../public/exercise.html'));
+    });
+
+    // routes to specific type of exercise
+    app.get('exercise/id', (request, response) => {
+        response.sendFile(path.join(__dirname, '../public/exercise.html'));
+    });
+
+    // routes to stats page
+    app.get('stats', (request, response) => {
+        response.sendFile(path.join(__dirname, '../public/stats.html'));
+    });
+};
